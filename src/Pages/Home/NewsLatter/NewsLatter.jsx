c71bf15b65e57918bf1f20dad41ec75791670f4a
anikmk <anikmollik122321@gmail.com>
2023-11-26 03:05:25 +0600

    news latter data send to the db

--- src/Pages/Home/NewsLatter/NewsLatter.jsx
+++ src/Pages/Home/NewsLatter/NewsLatter.jsx
@@ -1,8 +1,30 @@
-import { FaArrowTrendUp } from "react-icons/fa6";
+
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import newslatter from "../../../assets/home/newslatter/newslatter.svg";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const NewsLatter = () => {
+  const axiosSecure = useAxiosSecure();
+  const newsLatterInfo = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const newsLatterData = {name,email}
+    axiosSecure.post('/newslatter',newsLatterData)
+    .then(res=>{
+      if(res.data.insertedId){
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Subscribed",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    })
+    // console.log(newsLatterData)
+  }
   return (
     <div className="bg-base-300 py-12 border-b-2 border-[#80f221]">
       <div className="w-[80%] mx-auto">
@@ -16,10 +38,11 @@ const NewsLatter = () => {
               subHeading="subscrive to our newslatter and stay update"
             ></SectionTitle>
             <div className="card shrink-0 w-full shadow-2xl bg-base-100">
-              <form className="card-body">
+              <form onSubmit={newsLatterInfo} className="card-body">
                 <div className="form-control">
                   <input
                     type="text"
+                    name="name"
                     placeholder="Enter Your Name.."
                     className="input input-bordered"
                     required
@@ -28,14 +51,13 @@ const NewsLatter = () => {
                 <div className="form-control">
                   <input
                     type="email"
+                    name="email"
                     placeholder="Enter Your Email.."
                     className="input input-bordered"
                     required
                   />
-                </div>
-                <div className="">
-                <button className="mt-4 px-6 py-3 bg-[#80f221] text-black rounded-full font-semibold flex items-center text-xl hover:shadow-[#7ff2219a] shadow-xl hover:bg-[#7ff22146] text-center"> Subscrive Now<FaArrowTrendUp  className="ml-2"/></button>
-                </div>
+                </div> 
+                <input type="submit" value='Subscrive Now' className="btn btn-primary"></input>
                 </form>
               
             </div>
