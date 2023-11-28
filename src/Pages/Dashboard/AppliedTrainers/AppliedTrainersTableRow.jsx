6d6c92c5b05712e95f332512a434372090b0bc2f
anikmk <anikmollik122321@gmail.com>
2023-11-28 18:04:04 +0600

    make dashboard applied trainer page

--- src/Pages/Dashboard/AppliedTrainers/AppliedTrainersTableRow.jsx
+++ src/Pages/Dashboard/AppliedTrainers/AppliedTrainersTableRow.jsx
@@ -1,5 +1,6 @@
 
 const AppliedTrainersTableRow = () => {
+    
     return (
         <div>
             
--- src/Pages/Dashboard/AppliedTrainers/AppliendTrainers.jsx
+++ src/Pages/Dashboard/AppliedTrainers/AppliendTrainers.jsx
@@ -1,4 +1,5 @@
 import useAppliedTrainers from "../../../hooks/useAppliedTrainers";
+import AppliedTrainersTableRow from "./AppliedTrainersTableRow";
 
 
 const AppliendTrainers = () => {
@@ -12,18 +13,41 @@ const AppliendTrainers = () => {
     <thead>
       <tr>
         <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
+        Numbers
         </th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
+        <th>Imgae</th>
+        <th>Trainer Name</th>
+        <th>Email</th>
+        <th>Action</th>
       </tr>
     </thead>
     <tbody>
-        
+        {
+           appliedTrainers.map((appliedTrainer,index)=><tr
+           key={appliedTrainer._id}>
+            <th>
+              <label>
+                {index + 1}
+              </label>
+            </th>
+            <td>
+              <div className="flex items-center gap-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img src={ appliedTrainer.profileImage} alt="Avatar Tailwind CSS Component" />
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td>
+             {appliedTrainer.trainerName}
+            </td>
+            <td> {appliedTrainer.email} </td>
+            <th>
+              <button className="btn btn-ghost btn-xs"> {appliedTrainer.status} </button>
+            </th>
+          </tr>)
+        }
 
     </tbody>
 
